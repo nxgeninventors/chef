ab5e8982a8b581b5d837b36d38863f3f1b16088d
muni <muni2explore@gmail.com>
2022-11-01 16:10:15 +0530

    Fix

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="mt-10 sm:mt-0 mx-auto p-4 sm:p-6 lg:p-8">
-        @include('components.clients.right')
+        @include('clients.right')
         <div class="inline-block overflow-hidden min-w-full rounded-lg shadow">
             <table class="min-w-full leading-normal">
                 <thead>
